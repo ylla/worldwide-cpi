7735a6c74660f862046fb9435e9dfc3b20618eef
ylla <git@vonmalmborg.com>
2015-07-31 03:34:00 -0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "CPI Worldwide"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | CPI Worldwide"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | CPI Worldwide"
   end
-
 end
